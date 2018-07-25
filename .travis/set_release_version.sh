3f429c813498a2c5bb0eba78273a0cb8593e3734
Robert Csakany <robert.csakany@blackbelt.hu>
2018-07-25 17:33:05 +0200

    Warning message can cause the problem?

--- .travis/set_release_version.sh
+++ .travis/set_release_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 set -e
+
 export POJECT_BUILD_NUMBER=$(printf "%05d" $TRAVIS_BUILD_NUMBER)
 echo "Build: $PROJECT_BUILD_NUMBER"
 export PROJECT_VERSION=$(mvn help:evaluate --settings .maven.xml -N -Dexpression=project.version | grep -v '\[' | sed "s/-SNAPSHOT/-b$PROJECT_BUILD_NUMBER/")
