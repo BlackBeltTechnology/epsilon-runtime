9862f94f4ea62a2ce3169f01242584a696d8b379
Robert Csakany <robert.csakany@blackbelt.hu>
2018-07-25 16:53:15 +0200

    Fixing travis syntax

--- .travis/set_release_version.sh
+++ .travis/set_release_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
-export PROJECT_VERSION=$(mvn help:evaluate -N -Dexpression=project.version | grep -v '\[' | sed "s/-SNAPSHOT/-$TRAVIS_BUILD_NUMBER/")
+export POJECT_BUILD_NUMBER=$(printf "%05d" $TRAVIS_BUILD_NUMBER)
+export PROJECT_VERSION=$(mvn help:evaluate -N -Dexpression=project.version | grep -v '\[' | sed "s/-SNAPSHOT/-b$PROJECT_BUILD_NUMBER/")
 mvn versions:set -DnewVersion=$PROJECT_VERSION -f ./epsilon-runtime-parent/pom.xml
 mvn versions:set -DnewVersion=$PROJECT_VERSION
