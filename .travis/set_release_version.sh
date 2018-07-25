ba9f0c4c61f9a1aa9c4f7ab76a8ab90e65e62a9f
Robert Csakany <robert.csakany@blackbelt.hu>
2018-07-25 17:11:21 +0200

    Trying with the given settings.xml

--- .travis/set_release_version.sh
+++ .travis/set_release_version.sh
@@ -2,6 +2,6 @@
 
 set -e
 export POJECT_BUILD_NUMBER=$(printf "%05d" $TRAVIS_BUILD_NUMBER)
-export PROJECT_VERSION=$(mvn help:evaluate -N -Dexpression=project.version | grep -v '\[' | sed "s/-SNAPSHOT/-b$PROJECT_BUILD_NUMBER/")
-mvn versions:set -DnewVersion=$PROJECT_VERSION -f ./epsilon-runtime-parent/pom.xml
-mvn versions:set -DnewVersion=$PROJECT_VERSION
+export PROJECT_VERSION=$(mvn help:evaluate --settings .maven.xml -N -Dexpression=project.version | grep -v '\[' | sed "s/-SNAPSHOT/-b$PROJECT_BUILD_NUMBER/")
+mvn versions:set --settings .maven.xml -DnewVersion=$PROJECT_VERSION -f ./epsilon-runtime-parent/pom.xml
+mvn versions:set --settings .maven.xml -DnewVersion=$PROJECT_VERSION
