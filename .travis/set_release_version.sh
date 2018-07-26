e5285dda985b5d66b8949167efdbf8c369d7a54a
Robert Csakany <csakany.robert@gmail.com>
2018-07-26 03:31:27 +0200

    Moving to wercker

--- .travis/set_release_version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-export POJECT_BUILD_NUMBER=$(printf "%05d" $TRAVIS_BUILD_NUMBER)
-echo "Build: $PROJECT_BUILD_NUMBER"
-export PROJECT_VERSION=$(mvn help:evaluate --settings .maven.xml -N -Dexpression=project.version | grep -v '\[' | sed "s/-SNAPSHOT/-b$PROJECT_BUILD_NUMBER/")
-echo "Version: $PROJECT_VERSIONs"
-mvn versions:set --settings .maven.xml -DnewVersion=$PROJECT_VERSION -f ./epsilon-runtime-parent/pom.xml
-mvn versions:set --settings .maven.xml -DnewVersion=$PROJECT_VERSION
--- .travis/setup_gpg.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
